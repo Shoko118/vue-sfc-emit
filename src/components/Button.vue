a0cec88a26f35a1dce10d112f3167b7cf5dba622
Shoko118 <secast118@gmail.com>
2022-01-22 18:08:27 +0800

    update

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="getOpenButton">Open the damn button</button>
+</template>
+
+<script setup>
+const emit = defineEmits(["openButton"]);
+function getOpenButton() {
+  emit("openButton");
+}
+</script>
